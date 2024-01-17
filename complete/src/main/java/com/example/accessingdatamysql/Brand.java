0df329900ed367c863915bf1b83bd9294bb78091
AU4U <AU4U@users.noreply.github.com>
2024-01-18 01:39:32 +0800

    添加mysql驱动 添加品牌实体

---
+++ complete/src/main/java/com/example/accessingdatamysql/Brand.java
@@ -0,0 +1,33 @@
+package com.example.accessingdatamysql;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ * 品牌字典
+ */
+@Entity
+public class Brand {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
