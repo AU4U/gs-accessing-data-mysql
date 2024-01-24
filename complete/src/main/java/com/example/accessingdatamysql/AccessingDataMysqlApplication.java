1bfec7086bd0ef715b62b34b36db948cc21f2f57
AU4U <AU4U@users.noreply.github.com>
2024-01-25 00:16:03 +0800

    修复由于没有上传文件目录而不能获取到文件的bug

--- complete/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
+++ complete/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -5,11 +5,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import java.io.File;
+
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 public class AccessingDataMysqlApplication {
 
 	public static void main(String[] args) {
+		//当前文件夹下是否有upload-dir文件夹，没有则创建
+		//文件连接符号在java中用那个类来表示
+		//
+		File file = new File("."+File.separator+"upload-dir");
+		if (!file.exists()) {
+			file.mkdir();
+		}
 		SpringApplication.run(AccessingDataMysqlApplication.class, args);
 	}
 }
